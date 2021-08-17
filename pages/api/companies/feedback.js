95c1a4e6b236c424209b54f1be8acccadee68cc9
dev <devmaxup@gmail.com>
2021-08-17 11:57:21 +0300

    Feedback form

--- pages/api/companies/feedback.js
+++ pages/api/companies/feedback.js
@@ -19,6 +19,7 @@ export default async function handler(req, res) {
       status: 'success',
     });
   } catch (error) {
+    console.log(error);
     return res.status(500).json({ status: 'fail', message: error });
   }
 }
