63916c3a37e54758f14d9b5c507d42b822aa1bef
dev <devmaxup@gmail.com>
2021-11-19 13:31:14 +0200

    add disclamer page title

--- pages/disclamer/index.jsx
+++ pages/disclamer/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 
 import axios from 'axios';
 
@@ -8,6 +9,13 @@ import classes from './styles.module.scss';
 export default function DisclamerPage({ pageContent }) {
   return (
     <div className={classes.DisclamerPage}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1"
+        />
+        <title>Similo Disclamer</title>
+      </Head>
       <main dangerouslySetInnerHTML={{ __html: pageContent }} />
     </div>
   );
